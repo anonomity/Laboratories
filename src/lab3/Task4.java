efe755b973d60eaf5157b877f0c682f4ed1896da
Jackie <jlauravelez@gmail.com>
2018-10-23 22:05:31 +0200

    Failure #2
    discovering number converter, thus deleting its usage and understanding locale plug gitIgnore to ignore test classes

--- src/lab3/Task4.java
+++ src/lab3/Task4.java
@@ -1,29 +1,42 @@
 package lab3;
 
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Task4 {
 	
-	private int dayMonth;
+	private int day;
+	private int month;
 	private int year;
 	
 	public Task4(String line) throws Exception {
-		Pattern pat = Pattern.compile("(\\d{2}\\/){2}(\\d{4})");
+		Pattern pat = Pattern.compile("(\\d{2}\\/)(\\d{2}\\/)(\\d{4})");
 		Matcher m= pat.matcher(line);
 		if(!m.find()) {
 			throw new Exception();
 		}
 		try {
 			String field=m.group(1);
-			dayMonth=Integer.parseInt(field);
+			day=Integer.parseInt(field);
 			field=m.group(2);
+			month =Integer.parseInt(field);
+			field=m.group(3);
 			year =Integer.parseInt(field);
 		} catch (Exception ex) {
 			throw new Exception();
 		}
 	}
-	
+	public int getDay() {
+		return(day);
+	}
+	public int getMonth() {
+		return(month);
+	}
+	public int getYear() {
+		return(year);
+	}
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
