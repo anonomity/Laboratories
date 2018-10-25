839344d93d1fb3b08be3fd8d4964dfe72ea7024a
Jackie <jlauravelez@gmail.com>
2018-10-25 07:28:45 +0200

    certainties and uncertainties
    idk the program runs fine but when i debug there is some type of error when pattern needs to compile. Also after using this.Line1 it returns the # input, i think thats why it started doing that. And im dumb and used && instead of || so now its notifying user when wrong input was submitted

--- src/regex/Main.java
+++ src/regex/Main.java
@@ -1,6 +1,6 @@
 package regex;
 
-import java.util.Scanner;
+
 
 
 
@@ -8,13 +8,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Please insert PESEL");
-		Scanner s = new Scanner(System.in);
-		String input = s.nextLine();
-		Verify first = new Verify(input);
-		System.out.print(first.getVerify());
+		System.out.print("Please insert PESEL\t");
+		String try1 = "9822344234";
+		Verify first = new Verify(try1);
+		System.out.println(first.getVerify());
+		System.out.println(first.size());
 		
-		s.close();
+		
 	}
 
 }
--- src/regex/Verify.java
+++ src/regex/Verify.java
@@ -1,28 +1,37 @@
 package regex;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Verify {
 	
-	private String verify;
+	private String out;
 	private int newL;
-	private String PESEL;
+	
+	private String Line1;
 	
 	Verify(String Line)   {
-	Pattern p = Pattern.compile("(\\d.){11}");
-	/*Matcher m = p.matcher(Line);*/
+	this.Line1 = Line;
+	String regex = "\\d{11}";
+	Pattern p = Pattern.compile(regex);
+	Matcher m = p.matcher(Line);
+	boolean matches = m.matches();
 	newL = Line.length();
-	if( 11 > newL && newL > 11) {
-		verify = "wrong input"; 
+	if( 11 > newL || newL > 11) {
+		out = "wrong input"; 
 	}
 	else {
-		verify = PESEL;
+		out = Line;
+		
 	}
 	}
+
 
 	public String getVerify() {
-		return verify;
+		return(out);
 		}
-
+	public int size() {
+		return(newL);
+	}
 
 }
